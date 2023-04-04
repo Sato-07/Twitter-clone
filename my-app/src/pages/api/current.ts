00d8aaba73b377aae59588d110cc1d15e737d6a1
Sato-07 <120731915+Sato-07@users.noreply.github.com>
2023-04-04 02:24:12 +0200

    feat: Create fetcher function using Axios for API requests

--- my-app/src/pages/api/current.ts
+++ my-app/src/pages/api/current.ts
@@ -1,3 +1,4 @@
+import serverAuth from "@/libs/serverAuth";
 import { NextApiRequest, NextApiResponse } from "next/server";
 
 
@@ -10,6 +11,8 @@ export default async function handler(
             return res.status(405).end();
         }
     try{
+        const { currentUser } = await serverAuth(req)
+        return res.status(200).json(currentUser)
 
     }catch(error){
         console.log(error)

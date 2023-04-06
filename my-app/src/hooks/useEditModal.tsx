c38cb7b70bfdb72df306b6e8c92cd80b8667cf55
Sato-07 <120731915+Sato-07@users.noreply.github.com>
2023-04-06 02:22:22 +0200

    feat:Create Zustand store for hook edit modal state

---
+++ my-app/src/hooks/useEditModal.tsx
@@ -0,0 +1,18 @@
+import {create } from 'zustand';
+
+interface EditModalStore{
+    isOpen: boolean;
+    onOpen:() => void;
+    onClose:() => void;
+};
+
+
+const UseEditModal = create<EditModalStore>((set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false })
+}
+))
+
+export default UseEditModal
+

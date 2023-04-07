572f8768fddc99d2a2b8a71c07237951d955de81
Sato-07 <120731915+Sato-07@users.noreply.github.com>
2023-04-08 00:59:28 +0200

    feat:Add PostItem component with user details, user navigation, and like functionality

--- my-app/src/components/posts/PostFeed.tsx
+++ my-app/src/components/posts/PostFeed.tsx
@@ -1,26 +1,21 @@
-import usePosts from "@/hooks/usePosts";
-import PostItem from "./PostItem";
+import usePosts from '@/hooks/usePosts';
 
-interface PostFeedProps{
-    userId?:string;
+import PostItem from './PostItem';
 
+interface PostFeedProps {
+  userId?: string;
 }
 
-const PostFeed: React.FC<PostFeedProps> = ({userId}) => {
+const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+  const { data: posts = [] } = usePosts(userId);
 
-    const { data : posts = [] } = usePosts(userId as string)
+  return (
+    <>
+      {posts.map((post: Record<string, any>,) => (
+        <PostItem userId={userId} key={post.id} data={post} />
+      ))}
+    </>
+  );
+};
 
-    return(
-        <>
-        {posts.map((post: Record<string, any>) => (
-            <PostItem
-            userId={userId}
-            key={post.id}
-            data={post}
-            />
-        ))}
-
-        </>
-        )
-}
-export default PostFeed
+export default PostFeed;
--- my-app/src/components/posts/PostItem.tsx
+++ my-app/src/components/posts/PostItem.tsx
@@ -1,6 +1,53 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import UseLoginModal from "@/hooks/userLoginModal";
+import { formatDistanceToNowStrict } from "date-fns";
+import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+
+interface PostItemProps{
+    userId?:string;
+    data: Record<string, any>;
+}
 
 
-const PostItem = () =>{
+
+const PostItem: React.FC<PostItemProps> = ({ userId, data ={} }) =>{
+
+    const router = useRouter();
+    const loginModal = UseLoginModal()
+
+    const { data : currentUser } = useCurrentUser()
+    const goToUser = useCallback((event:any)=>{
+        event.stopPropagation()
+
+        router.push(`/users/${data.id}`)
+
+    },[router,data.id])
+
+    const onLike = useCallback((event:any)=>{
+        event.stopPropagation();
+
+        loginModal.onOpen()
+
+    
+
+    },[loginModal])
+
+    const createdAt = useMemo(()=>{
+
+        if(data?.createdAt){
+            return null
+        }
+
+        return formatDistanceToNowStrict(new Date(data?.createdAt))
+
+    },[data?.createdAt])
+
+
+
+
+
+
     return(<div></div>)
 }
 export default PostItem

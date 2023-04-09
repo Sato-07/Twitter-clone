e26cc1223f23b3a8a4a34a60087e01afda8c430a
Sato-07 <120731915+Sato-07@users.noreply.github.com>
2023-04-10 00:59:51 +0200

    feat:Added server-side API endpoint for creating comments with authentication and error handling.

--- my-app/src/components/posts/PostItem.tsx
+++ my-app/src/components/posts/PostItem.tsx
@@ -95,9 +95,9 @@ const PostItem: React.FC<PostItemProps> = ({ userId, data ={} }) =>{
 
                     </div>
                     <div onClick={onLike} className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500">
-                        <LikeIcon size={20}/>
+                        <LikeIcon size={20} color={hasLiked ? 'red' : ''}/>
                         <p>
-                            {data.like?.length || 0}
+                            {data.likedIds?.length}
                         </p>
 
                     </div>

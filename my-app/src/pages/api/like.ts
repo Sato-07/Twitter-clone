d7923ca5aae608921982ff4a73a9e7a7490b6c96
Sato-07 <120731915+Sato-07@users.noreply.github.com>
2023-04-09 23:20:53 +0200

    feat:Added custom hook for handling likes with server-side API calls and authentication.

--- my-app/src/pages/api/like.ts
+++ my-app/src/pages/api/like.ts
@@ -47,7 +47,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse){
                 likedIds: updatedLikedIds
             }
         })
-
+        return res.status(200).json(updatedPost)
     }catch(error){
         console.log(error)
         return res.status(400).end()
